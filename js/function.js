362273fdc64addca661bb415a9c3e5db2c10b52b
Lucho <luciomartinelli1@gmail.com>
2023-02-23 00:42:28 -0300

    cambios en evento boton function.js

---
+++ js/function.js
@@ -0,0 +1,7 @@
+document.getElementById("boton-contacto").addEventListener ('click', function() {
+console.log ("Mail enviado");
+//aca va la funcion de crear el pop upp con alerta de mail enviado// 
+
+}
+
+// despues hay que crear evento que efectivamente envie el mail//
