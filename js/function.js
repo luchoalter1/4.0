d33e53f6c81a0efe7b99db1cdb151f8bc36edfc4
Lucho <luciomartinelli1@gmail.com>
2023-02-26 00:48:44 -0300

    saasd

--- js/function.js
+++ js/function.js
@@ -1,5 +1,5 @@
-document.getElementById("logbutton").addEventListener ('click', function() {
-    console.log ("Mail enviado");//
+document.getElementById("logbutton").addEventListener ('click', function()) {
+    if document.getElementById("logbutton") = true
 //aca va la funcion de crear el pop upp con alerta de mail enviado// 
 
 }
