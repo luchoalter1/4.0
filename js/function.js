744974af0c9dbd69c8a254e82167bb8e07addee4
Lucho <luciomartinelli1@gmail.com>
2023-02-26 00:33:42 -0300

    login alert

--- js/function.js
+++ js/function.js
@@ -1,7 +1,6 @@
-//document.getElementById("boton-contacto").addEventListener ('click', function() {
-//console.log ("Mail enviado");//
+document.getElementById("logbutton").addEventListener ('click', function() {
+    console.log ("Mail enviado");//
 //aca va la funcion de crear el pop upp con alerta de mail enviado// 
 
 }
 
-// despues hay que crear evento que efectivamente envie el mail//
