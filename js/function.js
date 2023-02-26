e5b03abb6185ebb63bad2300556c2fb773b45c86
Lucho <luciomartinelli1@gmail.com>
2023-02-26 10:21:14 -0300

    login ingreso

--- js/function.js
+++ js/function.js
@@ -1,6 +1,15 @@
-document.getElementById("logbutton").addEventListener ('click', function()) {
-    if document.getElementById("logbutton") = true
+//document.getElementById("logbutton").addEventListener ('click', function()) {
+    //if document.getElementById("logbutton") = true
 //aca va la funcion de crear el pop upp con alerta de mail enviado// 
+
+function login() {
+    var usuario, password;
+    usuario = document.getElementById("usuario").value ;
+    password = document.getElementById("password").value ;
+
+    if (usuario == "lucio" && password == "1234") {
+        window.location = "ejemplo.html";
+    }
 
 }
 
